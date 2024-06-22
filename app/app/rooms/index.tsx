c5f4bf460cbbdf3fed0098817af8e6485303b5db
BraveVlad <giladvladpinker@gmail.com>
2024-06-23 02:29:16 +0300

    App/RoomsScreen - + mock data for rooms list view

--- app/app/rooms/index.tsx
+++ app/app/rooms/index.tsx
@@ -1,5 +1,29 @@
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, FlatList, StyleSheet, Text, View } from "react-native";
+import { Rooms } from "../../models/Room.model";
 
+const MOCK_ROOMS: Rooms = [
+	{
+		id: "1",
+		title: "first mock room",
+		creationDate: new Date(),
+		creator: undefined,
+		treasures: [],
+	},
+	{
+		id: "2",
+		title: "second mock room",
+		creationDate: new Date(),
+		creator: undefined,
+		treasures: [],
+	},
+	{
+		id: "3",
+		title: "third mock room",
+		creationDate: new Date(),
+		creator: undefined,
+		treasures: [],
+	},
+];
 export default function RoomsScreen() {
 	return (
 		<View style={styles.container}>
