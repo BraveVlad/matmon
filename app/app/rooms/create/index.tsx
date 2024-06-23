2c048e27d479a96131f0c350af3516a7bf96c81c
yizhak148 <yizhak208@gmail.com>
2024-06-23 15:48:10 +0300

    h

--- app/app/rooms/create/index.tsx
+++ app/app/rooms/create/index.tsx
@@ -1,6 +1,6 @@
 import { Link } from "expo-router";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import MapView from 'react-native-maps';;
+import MapView from 'react-native-maps';
 
 export default function RoomCreationScreen() {
 	return (
