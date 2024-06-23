24e33a9acd729959699f762edcdca4ee47213a7c
BraveVlad <giladvladpinker@gmail.com>
2024-06-23 04:25:27 +0300

    App/RoomsListView - + added support to query's isloading and iserror statuses. + fetchRooms mocks fetch data until axios is installed.

--- app/components/RoomsListView.tsx
+++ app/components/RoomsListView.tsx
@@ -1,4 +1,11 @@
-import { FlatList, ListRenderItemInfo, StyleSheet } from "react-native";
+import {
+	FlatList,
+	ListRenderItemInfo,
+	Pressable,
+	StyleSheet,
+	Text,
+	View,
+} from "react-native";
 import { Rooms, Room } from "../models/Room.model";
 import RoomsListViewItem from "./RoomsListViewItem";
 import { useQuery } from "@tanstack/react-query";
@@ -27,12 +34,15 @@ const MOCK_ROOMS: Rooms = [
 	},
 ];
 
-function fetchRooms() {
-	return MOCK_ROOMS;
+async function fetchRooms() {
+	const result = await new Promise<Rooms>((resolve) =>
+		setTimeout(() => resolve(MOCK_ROOMS), 2000)
+	);
+	return result;
 }
 
 export default function RoomsListView() {
-	const roomsQuery = useQuery({
+	const { data, isLoading, isError, refetch } = useQuery({
 		queryKey: ["rooms"],
 		queryFn: fetchRooms,
 	});
@@ -41,10 +51,23 @@ export default function RoomsListView() {
 		return <RoomsListViewItem room={item} />;
 	}
 
+	if (isLoading) {
+		return <Text>Loading rooms...</Text>;
+	}
+
+	if (isError) {
+		return (
+			<Pressable style={styles.errorMessage} onPress={() => refetch()}>
+				<Text>Oopsie! unable to fetch rooms!</Text>
+				<Text>Click here to reload 🔄️</Text>
+			</Pressable>
+		);
+	}
+
 	return (
 		<FlatList
 			style={styles.roomsList}
-			data={roomsQuery.data}
+			data={data}
 			renderItem={RenderRoomsListItem}
 			keyExtractor={(room) => room.id}
 		/>
@@ -55,5 +78,8 @@ const styles = StyleSheet.create({
 	roomsList: {
 		width: "75%",
 		gap: 16,
+	},
+	errorMessage: {
+		alignItems: "center",
 	},
 });
