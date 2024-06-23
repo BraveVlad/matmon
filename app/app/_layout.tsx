c39c55734fcddf57f2f55e6659dc62a1ea557da3
BraveVlad <giladvladpinker@gmail.com>
2024-06-23 03:24:01 +0300

    App/ReactQuery - + wrapped root layout with QueryClient

--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -1,5 +1,12 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Slot } from "expo-router";
 
+const queryClient = new QueryClient();
+
 export default function RootLayout() {
-	return <Slot />;
+	return (
+		<QueryClientProvider client={queryClient}>
+			<Slot />
+		</QueryClientProvider>
+	);
 }
