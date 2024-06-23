c177c7049059816947e7ce593f334b7b6a3edefc
BraveVlad <giladvladpinker@gmail.com>
2024-06-23 23:42:16 +0300

    App/CreateRoomScreen - + Changed screen layout according to design file. + added main actions bar (save/start) + added basic map view component to show treasures of the room named TreasureMapView

--- app/app/rooms/create/index.tsx
+++ app/app/rooms/create/index.tsx
@@ -1,31 +1,60 @@
 import { Link } from "expo-router";
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import { useRef } from "react";
+import {
+	Button,
+	Platform,
+	Pressable,
+	StyleSheet,
+	Text,
+	View,
+} from "react-native";
+import MapView from "react-native-maps";
 
 export default function RoomCreationScreen() {
-  return (
-    <View style={styles.container}>
-      <Text>Welcome to Room Creation Screen!</Text>
-      <Pressable>
-        <Link asChild href="">
-          create
-        </Link>
-      </Pressable>
-      <Pressable>
-        <Link asChild href="">
-          start
-        </Link>
-      </Pressable>
-	  {/* map view here */}
-	  {/* modal of trusere here */}
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Text>Welcome to Room Creation Screen!</Text>
+			<View style={styles.actionsBar}>
+				<Button title="Save" />
+				<Button title="Start" />
+			</View>
+			<TreasuresMapView />
+		</View>
+	);
+}
+
+function TreasuresMapView() {
+	const mapRef = useRef<MapView>(null);
+
+	if (Platform.OS === "web") {
+		return (
+			<View style={styles.mapErrorContainer}>
+				<Text>Map is not supported on web.</Text>
+			</View>
+		);
+	}
+	return (
+		<View style={styles.mapContainer}>
+			<MapView ref={mapRef} />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	actionsBar: {},
+	mapContainer: {
+		width: 250,
+		height: 250,
+	},
+	mapErrorContainer: {
+		alignContent: "center",
+		justifyContent: "center",
+		backgroundColor: "red",
+	},
 });
