7b76e60623592b1f05393247b1d73cc11fb4fb7a
Yotam Adler <yotam.adler@gmail.com>
2024-06-24 23:01:29 +0300

    added very basic modal code

--- app/components/creator/TreasureCreateModalButton.tsx
+++ app/components/creator/TreasureCreateModalButton.tsx
@@ -1,13 +1,43 @@
 import { router } from "expo-router";
-import { View, Button } from "react-native";
+import { View, Button, useState, Modal, Google } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
 export default function TreasureCreateModalButton() {
+
 	return (
-		<View>
-			<Button
-				title="Add Treasure"
-				onPress={() => router.push("/creator/treasure")}
-			/>
-		</View>
+	<View>
+		<Modal> 
+			<MapView
+			provider={PROVIDER_GOOGLE} 
+			style={styles.map}
+			initialRegion={{
+			latitude: 37.78825,
+			longitude: -122.4324,
+			latitudeDelta: 0.0922,
+			longitudeDelta: 0.0421,
+			}}
+			/> 
+
+			<div class="grid-container">
+        		<input type="text" placeholder="Enter text here">
+        		<button>Submit</button>
+  			</div>
+
+		</Modal>
+	</View>
 	);
 }
+
+const styles = StyleSheet.create({
+	grid-container {
+		display: grid;
+		grid-template-columns: auto auto;
+		align-items: center; \
+		gap: 10px; 
+	},
+
+	map: {
+		...StyleSheet.absoluteFillObject,
+	},
+});
+
