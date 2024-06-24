5f5e26f69291f2ed692d37d359d423fe636d4713
Dracula <giladvladpinker@gmail.com>
2024-06-24 06:10:05 +0300

    App/CreatorScreen (#22)
    * App/CreateRoomScreen -
    + Changed screen layout according to design file.
    + added main actions bar (save/start)
    + added basic map view component to show treasures of the room named TreasureMapView
    + Changed to fixed MapView implementation
    + implemented basic treasures listview with mockdata
    + implemented basic room title edit component
    + added basic "Add Treasure" button that will open a modal.
    + extracted TreasuresListView to it's own file
    + extracted TreasuresMapView to it's own file
    + extracted RoomTitleInput to it's own file
    + extracted TreasureCreateModalButton to it's own file
    + moved 'creator' components directory from "models" to "components" dir.
    + renamed route 'create' to 'creator'. changed relevant navigation-actions.

---
+++ app/components/creator/TreasuresListView.tsx
@@ -0,0 +1,73 @@
+import {
+	ListRenderItemInfo,
+	View,
+	FlatList,
+	Text,
+	StyleSheet,
+} from "react-native";
+import { Treasures, Treasure } from "../../models/Treasure.model";
+
+export default function TreasuresListView() {
+	const MOCK_TREASURES: Treasures = [
+		{
+			id: "1a2b3c",
+			name: "Golden Crown",
+			searchRadius: 150,
+			isFound: false,
+			coordinate: {
+				latitude: 34.052235,
+				longitude: -118.243683,
+			},
+		},
+		{
+			id: "4d5e6f",
+			name: "Ancient Amulet",
+			searchRadius: 200,
+			isFound: true,
+			coordinate: {
+				latitude: 40.712776,
+				longitude: -74.005974,
+			},
+		},
+		{
+			id: "7g8h9i",
+			name: "Pirate's Chest",
+			searchRadius: 300,
+			isFound: false,
+			coordinate: {
+				latitude: 25.761681,
+				longitude: -80.191788,
+			},
+		},
+	];
+
+	function renderTreasureListItem({
+		item,
+		index,
+	}: ListRenderItemInfo<Treasure>) {
+		return (
+			<View>
+				<Text>
+					{index} ) Treasure # {item.name}
+				</Text>
+			</View>
+		);
+	}
+
+	return (
+		<View style={styles.treasuresList}>
+			<Text>Treasures List View</Text>
+			<FlatList
+				data={MOCK_TREASURES}
+				keyExtractor={(treasure) => treasure.id}
+				renderItem={renderTreasureListItem}
+			/>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	treasuresList: {
+		flex: 1,
+	},
+});
