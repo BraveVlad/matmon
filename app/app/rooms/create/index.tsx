4bbcc3acc99810bb30f71bf97943365492ce102b
BraveVlad <giladvladpinker@gmail.com>
2024-06-24 03:43:32 +0300

    App/CreateRoomScreen - + implemented basic treasures listview with mockdata

--- app/app/rooms/create/index.tsx
+++ app/app/rooms/create/index.tsx
@@ -2,6 +2,8 @@ import { Link } from "expo-router";
 import { useRef } from "react";
 import {
 	Button,
+	FlatList,
+	ListRenderItemInfo,
 	Platform,
 	Pressable,
 	StyleSheet,
@@ -9,6 +11,7 @@ import {
 	View,
 } from "react-native";
 import { MapView } from "../../../components/MapView/MapView";
+import { Treasure, Treasures } from "../../../models/Treasure.model";
 
 export default function RoomCreationScreen() {
 	return (
@@ -19,6 +22,66 @@ export default function RoomCreationScreen() {
 				<Button title="Start" />
 			</View>
 			<TreasuresMapView />
+			<TreasuresListView />
+		</View>
+	);
+}
+
+function TreasuresListView() {
+	const MOCK_TREASURES: Treasures = [
+		{
+			id: "1a2b3c",
+			name: "Golden Crown",
+			searchRadius: 150,
+			isFound: false,
+			coordinate: {
+				latitude: 34.052235,
+				longitude: -118.243683,
+			},
+		},
+		{
+			id: "4d5e6f",
+			name: "Ancient Amulet",
+			searchRadius: 200,
+			isFound: true,
+			coordinate: {
+				latitude: 40.712776,
+				longitude: -74.005974,
+			},
+		},
+		{
+			id: "7g8h9i",
+			name: "Pirate's Chest",
+			searchRadius: 300,
+			isFound: false,
+			coordinate: {
+				latitude: 25.761681,
+				longitude: -80.191788,
+			},
+		},
+	];
+
+	function renderTreasureListItem({
+		item,
+		index,
+	}: ListRenderItemInfo<Treasure>) {
+		return (
+			<View>
+				<Text>
+					{index} ) Treasure # {item.name}
+				</Text>
+			</View>
+		);
+	}
+
+	return (
+		<View>
+			<Text>Treasures List View</Text>
+			<FlatList
+				data={MOCK_TREASURES}
+				keyExtractor={(treasure) => treasure.id}
+				renderItem={renderTreasureListItem}
+			/>
 		</View>
 	);
 }
