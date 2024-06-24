616e41bd124db647b32183bbd10c1335dab2ba8a
Yotam Adler <yotam.adler@gmail.com>
2024-06-24 23:01:24 +0300

    added: very basic modal

--- app/app/creator/treasure.tsx
+++ app/app/creator/treasure.tsx
@@ -1,25 +1,11 @@
 import { Button, StyleSheet, Text, View, FlatList } from "react-native";
-import React from "React"
+import React, { useState } from "React"
 export default function CreateTreasureModal() {
 
-	const data = [
-		{ id: '1', title: 'Item 1' },
-		{ id: '2', title: 'Item 2' },
-		{ id: '3', title: 'Item 3' },
-		{ id: '4', title: 'Item 4' },
-		{ id: '5', title: 'Item 5' },
-	  ];
 
-	  const renderItem = ({ item }) => (
-		<View style={styles.item}>
-		  <Text style={styles.title}>{item.title}</Text>
-		</View>
-	  );
 
 	return (
-		<View style={styles.container}>
-			<Text>Create a Room</Text>
-		</View>
+
 	);
 }
 
