189f2adc62b185e9d00afaa3adb073ec091c475f
BraveVlad <giladvladpinker@gmail.com>
2024-06-24 03:05:58 +0300

    App/Navigation - + buttons in index page to navigate to create/join

--- app/app/index.tsx
+++ app/app/index.tsx
@@ -1,9 +1,14 @@
-import { StyleSheet, Text, View } from "react-native";
+import { router } from "expo-router";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
 	return (
 		<View style={styles.container}>
 			<Text>Welcome to Matmon app!</Text>
+			<View style={styles.actions}>
+				<Button title="JOIN" onPress={() => router.push("/game")} />
+				<Button title="CREATE" onPress={() => router.push("/rooms")} />
+			</View>
 		</View>
 	);
 }
@@ -14,5 +19,9 @@ const styles = StyleSheet.create({
 		backgroundColor: "#fff",
 		alignItems: "center",
 		justifyContent: "center",
+	},
+	actions: {
+		flexDirection: "row",
+		gap: 16,
 	},
 });
