7b12289a49a153c29817c6f4f69448d0a56e2cd1
BraveVlad <giladvladpinker@gmail.com>
2024-06-25 18:26:16 +0300

    Back/KeysRouter - + removed api keys router because google map's keys are protected.

--- api/src/index.ts
+++ api/src/index.ts
@@ -6,7 +6,6 @@ import express from "express";
 import cors from "cors";
 import { json } from "body-parser";
 import { router as RoomsRouter } from "./routers/Rooms.Router";
-import { router as KeysRouter } from "./routers/Keys.Router";
 
 const app = express();
 
@@ -14,7 +13,6 @@ app.use(cors());
 app.use(json());
 
 app.use("/rooms", RoomsRouter);
-app.use("/keys", KeysRouter);
 
 app.get("/check", async (_, res) => {
 	res.status(200);
@@ -24,10 +22,6 @@ app.get("/check", async (_, res) => {
 async function init() {
 	if (!process.env.MONGO_CONN_STRING) {
 		throw new Error("Must provide connection string");
-	}
-
-	if (!process.env.MAPS_API_KEY) {
-		console.error("WARNING - Maps api key not provided!");
 	}
 
 	// await mongoose.connect(process.env.MONGO_CONN_STRING, {
