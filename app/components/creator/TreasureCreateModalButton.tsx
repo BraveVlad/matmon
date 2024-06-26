7d3e5ed2ea91a1166056b52ff0c60fa872b9bad9
BraveVlad <giladvladpinker@gmail.com>
2024-06-27 01:13:39 +0300

    App/CreateTreasureModal - + Implemented CreateTreasureModal that opens and closes by a button that handles the modal visibility.

--- app/components/creator/TreasureCreateModalButton.tsx
+++ app/components/creator/TreasureCreateModalButton.tsx
@@ -1,13 +1,85 @@
 import { router } from "expo-router";
-import { View, Button } from "react-native";
+import {
+	View,
+	Button,
+	Text,
+	Modal,
+	StyleSheet,
+	BackHandler,
+} from "react-native";
+import { Treasure } from "../../models/Treasure.model";
+import { useState } from "react";
 
 export default function TreasureCreateModalButton() {
+	const [isShowTreasureModal, setIsShowTreasureModal] =
+		useState<boolean>(false);
+
+	function openCreateTreasureModal() {
+		console.log("Open treasure modal.");
+		setIsShowTreasureModal(true);
+	}
+
 	return (
 		<View>
-			<Button
-				title="Add Treasure"
-				onPress={() => router.push("/creator/treasure")}
+			<Button title="Add Treasure" onPress={openCreateTreasureModal} />
+			<CreateTreasureModal
+				isVisible={isShowTreasureModal}
+				onCancelled={() => {
+					setIsShowTreasureModal(false);
+				}}
+				onTreasureCreated={() => {}}
 			/>
 		</View>
 	);
 }
+
+type CreateTreasureModalProps = {
+	isVisible: boolean;
+	onCancelled: () => void;
+	onTreasureCreated: (treasure: Treasure) => void;
+};
+
+function CreateTreasureModal({
+	isVisible,
+	onCancelled,
+	onTreasureCreated,
+}: CreateTreasureModalProps) {
+	function handleOnClose() {
+		onCancelled();
+	}
+
+	return (
+		<Modal animationType="slide" transparent={true} visible={isVisible}>
+			<View style={styles.modal}>
+				<View style={styles.titleContainer}>
+					<Text style={styles.title}>Treasure modal title</Text>
+				</View>
+				<View style={styles.contentContainer}></View>
+				<View style={styles.actions}>
+					<Button title="close" onPress={handleOnClose} />
+					<Button title="create" />
+				</View>
+			</View>
+		</Modal>
+	);
+}
+
+const styles = StyleSheet.create({
+	modal: {
+		top: "25%",
+		height: "75%",
+		backgroundColor: "white",
+	},
+	titleContainer: {},
+	title: {},
+	contentContainer: {
+		flex: 9,
+		// height: "50%",
+	},
+	actions: {
+		flex: 1,
+		flexDirection: "row",
+		justifyContent: "space-around",
+	},
+	actionButton: {},
+});
