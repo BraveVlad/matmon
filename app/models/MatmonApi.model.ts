15398afe7a4cee8f579d3daa9387703ff606b36c
BraveVlad <giladvladpinker@gmail.com>
2024-06-27 03:08:56 +0300

    App/ApiModel- + updated postCreateRoomUri builder to include username

--- app/models/MatmonApi.model.ts
+++ app/models/MatmonApi.model.ts
@@ -8,8 +8,8 @@ export const MATMON_API = {
 	rooms: "/rooms",
 };
 
-export function getAllRoomsUri() {
-	return MATMON_API.base + MATMON_API.rooms + "/all";
+export function getAllRoomsUri(username: string) {
+	return MATMON_API.base + MATMON_API.rooms + `/${username}`;
 }
 
 export function postCreateRoomUri() {
