3e734935e1e95c0bc3a79b3e441244c01dbec7fc
Yotam Adler <yotam.adler@gmail.com>
2024-06-27 10:31:01 +0300

    After revert: - Added user model -Fixed tsconfig issue

--- app/models/Room.model.ts
+++ app/models/Room.model.ts
@@ -1,11 +1,18 @@
 import { Treasures } from "./Treasure.model";
 
+export type user = {
+	userId: string;
+	userName: string;
+	password: string;
+	activeRooms: string[];
+}
+
 export type Room = {
 	id: string;
 	title: string;
 	creationDate: Date;
 	treasures: Treasures;
-	creator: string; // TODO - Change to a User model
+	creator: string; // userId 
 };
 
 export type NewRoom = Omit<Room, "id" | "creationDate">;
