bb416cf9548934dafca39ef6c1035b15a26d4cde
BraveVlad <giladvladpinker@gmail.com>
2024-06-29 14:43:51 +0300

    App/CreateTreasure - + Added 'Circle' import to custom MapView component

--- app/components/MapView/MapView.tsx
+++ app/components/MapView/MapView.tsx
@@ -4,7 +4,8 @@ import MapView, {
 	Region,
 	MarkerDragStartEndEvent,
 	MapPressEvent,
+	Circle,
 } from "react-native-maps";
 
-export { MapView, Marker, PROVIDER_GOOGLE };
+export { MapView, Marker, Circle, PROVIDER_GOOGLE };
 export type { Region, MarkerDragStartEndEvent, MapPressEvent };
