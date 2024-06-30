047043126ba2f94c76f6610c5e3f05361f949fb8
Yotam Adler <yotam.adler@gmail.com>
2024-06-30 14:28:25 +0300

    installed tailwind

--- api/src/models/Room.model.ts
+++ api/src/models/Room.model.ts
@@ -1,20 +1,34 @@
-import { Treasures } from "./Treasure.model";
+import { Schema, Types, model } from "mongoose";
+import { Treasures, treasureSchema } from "./Treasure.model";
 
-type user = {
-	userId: string;
-	userName: string;
-	password: string;
-	activeGameIds: string[];  
-}
-
-export type Room = {
-	id: string;
+export interface Room {
 	title: string;
 	creationDate: Date;
 	treasures: Treasures;
-	creator: user;
-};
-
-export type NewRoom = Omit<Room, "id" | "creationDate">;
-
+	creator: string;
+}
+export type NewRoom = Omit<Room, "creationDate">;
 export type Rooms = Room[];
+
+const roomSchema = new Schema<Room>({
+	title: {
+		type: String,
+		requied: true,
+		maxlength: 25,
+		minLength: 5,
+	},
+	creationDate: {
+		type: Date,
+		requied: true,
+		default: Date.now,
+	},
+	treasures: [treasureSchema],
+	creator: {
+		type: String,
+		requied: true,
+		maxlength: 25,
+		minLength: 0,
+	},
+});
+
+export const Room = model<Room>("Room", roomSchema, "rooms");
