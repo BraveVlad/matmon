1cb5970b2e143183167d362230609fd38eae2fe9
Dracula <giladvladpinker@gmail.com>
2024-07-01 12:13:23 +0300

    App/netlify-migration (#62)
    * App/Web -
    - Migrating to Netlify

--- app/app.config.ts
+++ app/app.config.ts
@@ -38,7 +38,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
 	web: {
 		favicon: "./assets/favicon.png",
 		bundler: "metro",
-		output: "server",
+		output: "single",
 	},
 	experiments: {
 		baseUrl: "/matmon",
