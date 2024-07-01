a20e4fcd33e075b97de9f2b705cf86f1ee35e1b7
BraveVlad <giladvladpinker@gmail.com>
2024-07-01 04:20:30 +0300

    App/Room View - + Created 'print qr' modal which generates QRs based on room's treasures.

---
+++ app/components/room/PrintQrModalButton.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { View, Button, Modal } from "react-native";
+import PrintQrModal from "./PrintQrModal";
+import { Treasures } from "../../models/Treasure.model";
+
+type ModalButtonProps = {
+	roomId: string;
+	roomTitle?: string;
+	treasures?: Treasures;
+	isDisabled?: boolean;
+};
+
+export default function PrintQrModalButton({
+	roomId,
+	roomTitle,
+	treasures,
+	isDisabled,
+}: ModalButtonProps) {
+	const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+
+	function onOpen() {
+		setIsModalVisible(true);
+	}
+
+	function onClose() {
+		setIsModalVisible(false);
+	}
+	return (
+		<View>
+			<Button title="Print QR" onPress={onOpen} disabled={isDisabled} />
+			<PrintQrModal
+				isVisible={isModalVisible}
+				roomId={roomId}
+				roomTitle={roomTitle}
+				treasures={treasures}
+				onClose={onClose}
+			/>
+		</View>
+	);
+}
